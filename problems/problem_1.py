5a2e140e01a471b9ef738655078934ad153cfdfb
Edmond Kotowski <ekotowski@gmail.com>
2015-07-10 16:31:10 -0700

    Added problem 1 solution
    - Added problem 1 solution (Sum of multiple of 3 or 5 below 1000)

---
+++ problems/problem_1.py
@@ -0,0 +1,8 @@
+# Sum of multiples of 3 or 5 below 1000
+
+def sum_of_multiples():
+    return sum([x for x in range(0, 1000) if x % 3 == 0 or x % 5 == 0])
+
+if __name__ == "__main__":
+    print "Sum of multiple of 3 or 5 below 1000: {sum}".format(
+        sum=sum_of_multiples())
