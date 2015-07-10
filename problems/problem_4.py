abfe9fc388a5aaf6a6e8b255b8f5c04a27784cf6
Edmond Kotowski <ekotowski@gmail.com>
2015-07-10 16:03:56 -0700

    Inital project and problem 4 solution
    - Added project layout
    - Added problem 4 solution (Largest Palindrome product)

---
+++ problems/problem_4.py
@@ -0,0 +1,24 @@
+def is_palindrome(product):
+    product = str(product)
+    reverse = product[::-1]
+    return product == reverse
+
+
+def largest_palindrome_product():
+    left_value = 999
+    max_product = 0
+    while left_value > 0:
+        right_value = 999
+        while right_value > 0:
+            product = left_value * right_value
+            if is_palindrome(product):
+                if product > max_product:
+                    max_product = product
+            right_value -= 1
+        left_value -= 1
+
+    return max_product
+
+if __name__ == "__main__":
+    print "Largest palindrome product: {product}".format(
+        product=largest_palindrome_product())
